72f0ac258880425515b47a807948277147d27d1c
Michel Maia <programador.michel.maia@gmail.com>
2022-12-02 10:02:57 -0300

    commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Quiz com JS
- desenvolvido para aprendizado
+
+
+![preview](./.github/preview.png)
+
+
+Desenvolvido para aprendizado, realizado Quiz com Javascript. 
+
+
+
+[🔗 Clique aqui para acessar] (https://michel-maia.github.io/Quiz-com-JS/)
+
+
+## 💻 Tecnologias
+
+- HTML
+- CSS
+- Javascript
+
+
+## Referência
+
+- [youtube] https://www.youtube.com/@will_dev 
+
+
---
+++ js/questions.js
@@ -0,0 +1,99 @@
+export default [
+    {
+        question:"Qual nome correto ?",
+        answers: [
+            {option: "Naruto Uzumaki", correct: true},
+            {option: "Naruto Kishimoto", correct: false},
+            {option: "Naruto Gaara", correct: false}
+        ],
+    },
+
+    {
+        question:"Raposa de ...",
+        answers: [
+            {option: "9 caudas", correct: true},
+            {option: "7 caudas", correct: false},
+            {option: "Uma só", correct: false}
+        ],
+    },
+
+    {
+        question:"Aldeia da ...",
+        answers: [
+            {option: "Areia", correct: false},
+            {option: "Fogo", correct: false},
+            {option: "Folha", correct: true}
+        ],
+    },
+
+
+    {
+        question:"Mais inteligente que Einsten ?",
+        answers: [
+            {option: "Shikamaru", correct: true},
+            {option: "Naruto", correct: false},
+            {option: "Sasuke", correct: false}
+        ],
+    },
+
+
+    {
+        question:"Qual time Kakashi ?",
+        answers: [
+            {option: "10", correct: false},
+            {option: "7", correct: true},
+            {option: "1", correct: false}
+        ],
+    },
+
+
+    {
+        question:"Lembra o Bruce Lee ",
+        answers: [
+            {option: "Might Guy", correct: true},
+            {option: "Choji", correct: false},
+            {option: "Haku", correct: false}
+        ],
+    },
+
+
+    {
+        question:"Melhor médica do mundo ninja ?",
+        answers: [
+            {option: "Rock", correct: false},
+            {option: "Sakura", correct: true},
+            {option: "Sasuke", correct: false}
+        ],
+    },
+
+    {
+        question:"Qual o bordão de Naruto ?",
+        answers: [
+            {option: "tô certo", correct: true},
+            {option: "eita lele", correct: false},
+            {option: "tudo certo", correct: false}
+        ],
+    },
+
+
+    {
+        question:"Vilão amado",
+        answers: [
+            {option: "Kushina", correct: false},
+            {option: "Itachi Uchiha", correct: true},
+            {option: "Ninjutsu", correct: false}
+        ],
+    },
+
+
+    {
+        question:"Comida favorita do Naruto ?",
+        answers: [
+            {option: "Ramen", correct: true},
+            {option: "Feijoada", correct: false},
+            {option: "Yakisoba", correct: false}
+        ],
+    },
+
+
+]
